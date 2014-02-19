9356a7c1a9dee98fc580b930892e61a87ce76356
anonymint <mint.com@gmail.com>
2014-02-19 17:34:56 +1100

    Setup backup link for js files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,10 @@
 require.config({
 
 	paths: {
-		'jquery' : 'libs/jquery-1.11.0.min',
-		'bootstrap' : 'libs/bootstrap.min',
+		'jquery' : ['//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min','libs/jquery-1.11.0.min'],
+		'bootstrap' : ['//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min', 'libs/bootstrap.min'],
 		'google-analytics' : 'libs/ga',
-		'google-code-prettify' : 'libs/google-code-prettify/run_prettify',
+		'google-code-prettify' : ['//google-code-prettify.googlecode.com/svn/loader/run_prettify','libs/google-code-prettify/run_prettify'],
 		'add-this' : 'http://s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5303fb49013f26e0',
 		'add-this-me' : 'libs/addthis-me'				
 	},
