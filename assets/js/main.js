145b10cd94ed1698cf7c150a41f364a70e7cbe89
Anonymint <mint.com@gmail.com>
2016-08-18 11:24:48 +1000

    Fix run_pretty.js cdn and fallback file in requirejs

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ require.config({
 	paths: {
 		'jquery' : ['//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min','libs/jquery-1.11.0.min'],
 		'bootstrap' : ['//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min', 'libs/bootstrap.min'],
-		'google-code-prettify' : ['//google-code-prettify.googlecode.com/svn/loader/run_prettify','libs/google-code-prettify/run_prettify']				
+		'google-code-prettify' : ['//cdn.rawgit.com/google/code-prettify/master/loader/run_prettify','libs/google-code-prettify/run_prettify']				
 	},
 
 	shim: {
